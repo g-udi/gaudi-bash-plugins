0670f108f979964b21116950f3c6b3e4592ad461
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2020-10-05 20:17:44 -0700

    Adding bash-it plugins

---
+++ thefuck.plugins.bash
@@ -0,0 +1,10 @@
+# shellcheck disable=SC2046
+
+cite about-plugin
+about-plugin 'Initialization for fuck'
+
+# https://github.com/nvbn/thefuck
+
+if _command_exists thefuck; then
+  eval $(thefuck --alias)
+fi
